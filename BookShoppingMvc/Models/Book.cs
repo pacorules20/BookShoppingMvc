58b1b0226aa5664cdc971834539926fc73bdb823
ivan ferreyra <ferreyraivan779@gmail.com>
2024-09-04 19:47:24 -0300

    Agregar archivos de proyecto.

---
+++ BookShoppingMvc/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Antiforgery;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookShoppingMvc.Models
+{
+    [Table("Book")]
+    public class Book
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(40)]
+
+        public string BookName { get; set; }
+    }
+}
---
+++ BookShoppingMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookShoppingMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
